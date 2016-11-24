d2ae80d5029a9871e1731601318e0384aed49132
Günther Eder <geder@cosy.sbg.ac.at>
2016-11-24 11:16:18 +0100

    license added to source

--- add_slabs.py
+++ add_slabs.py
@@ -1,3 +1,24 @@
+"""
+    add_slabs.py is a Blender Add-On that computes and constructs edge and
+    'motorcycle' slabs from a given input polygon.
+    {one line to give the program's name and a brief idea of what it does.}
+    Copyright (C) 2016  Günther Eder  geder@cosy.sbg.ac.at
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 bl_info = {
     "name": "Add Edge/MC Slabs",
     "author": "Günther Eder",
